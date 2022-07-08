135a70662ba86a6bf3a7db7a9c43680b7015bef8
Yasin Gorgij <yasin.gorgij@proton.me>
2022-07-08 19:02:45 +0430

    Add project base structure

---
+++ lib/house_inventory.ex
@@ -0,0 +1,18 @@
+defmodule HouseInventory do
+  @moduledoc """
+  Documentation for `HouseInventory`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HouseInventory.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/house_inventory_test.exs
@@ -0,0 +1,8 @@
+defmodule HouseInventoryTest do
+  use ExUnit.Case
+  doctest HouseInventory
+
+  test "greets the world" do
+    assert HouseInventory.hello() == :world
+  end
+end
